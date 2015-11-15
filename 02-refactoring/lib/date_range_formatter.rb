de6b8cef6319cd87472a185d65d916d083348a23
Alice Wyan <alice@finitud.org>
2015-11-15 09:49:15 +0000

    Initial refactoring

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -9,50 +9,67 @@ class DateRangeFormatter
     @end_time = end_time
   end
 
+  def same_date_formatter(full_start_date, full_end_date)
+    if @start_time && @end_time
+      "#{full_start_date} at #{@start_time} to #{@end_time}"
+    elsif @start_time
+      "#{full_start_date} at #{@start_time}"
+    elsif @end_time
+      "#{full_start_date} until #{@end_time}"
+    else
+      full_start_date
+    end
+  end
+
+  def same_month_formatter(full_start_date, full_end_date)
+    if @start_time && @end_time
+      "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
+    elsif @start_time
+      "#{full_start_date} at #{@start_time} - #{full_end_date}"
+    elsif @end_time
+      "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    else
+      @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
+    end
+  end
+
+  def same_year_formatter(full_start_date, full_end_date)
+    if @start_time && @end_time
+      "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
+    elsif @start_time
+      "#{full_start_date} at #{@start_time} - #{full_end_date}"
+    elsif @end_time
+      "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    else
+      @start_date.strftime("#{@start_date.day.ordinalize} %B - ") +
+      @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+    end
+  end
+
+  def default_formatter(full_start_date, full_end_date)
+    if @start_time && @end_time
+      "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
+    elsif @start_time
+      "#{full_start_date} at #{@start_time} - #{full_end_date}"
+    elsif @end_time
+      "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    else
+      "#{full_start_date} - #{full_end_date}"
+    end
+  end
+
   def to_s
     full_start_date = @start_date.strftime("#{@start_date.day.ordinalize} %B %Y")
     full_end_date = @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
 
     if @start_date == @end_date
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} to #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time}"
-      elsif @end_time
-        "#{full_start_date} until #{@end_time}"
-      else
-        full_start_date
-      end
+      return same_date_formatter(full_start_date, full_end_date)
     elsif @start_date.month == @end_date.month
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
-      end
+      return same_month_formatter(full_start_date, full_end_date)
     elsif @start_date.year == @end_date.year
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
-      end
+      return same_year_formatter(full_start_date, full_end_date)
     else
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        "#{full_start_date} - #{full_end_date}"
-      end
+      return default_formatter(full_start_date, full_end_date)
     end
   end
 end
